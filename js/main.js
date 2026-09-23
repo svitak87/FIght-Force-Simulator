8de0e38dc5b42971f48308280402a7cad46b4929
Oscar Fajardo <svitakwe87@hotmail.com>
2026-09-23 18:27:41 -0300

    Implementando clases

--- js/main.js
+++ js/main.js
@@ -1,11 +1,82 @@
-let vidaJugador = 100;
-let vidaEnemigo = 100;
-let intentosDeCuracion = 0;
-let intentosDeAtaque = 0;
 let juegoEnMarcha = true;
-const ataques = ["Puño", "Patada", "Espadazo", "Hechizo", "Flecha"];
+
+class Jugador {
+  constructor(nombre) {
+    this.nombre = nombre;
+    this.intentosDeCuracion = 0;
+    this.intentosDeAtaque = 0;
+    this.vida = 100;
+    this.ataques = [
+      { nombre: "Puño", daño: 20 },
+      { nombre: "Patada", daño: 25 },
+      { nombre: "Espadazo", daño: 30 },
+      { nombre: "Hechizo", daño: 35 },
+      { nombre: "Flecha", daño: 40 },
+    ];
+  }
+  atacar(opcionAtaque) {
+    if (this.intentosDeAtaque > 2) {
+      alert("Ya no puedes atacar más. Selecciona sí o sí la opción 4.");
+      return;
+    }
+    if (this.intentosDeAtaque === 2) {
+      this.intentosDeAtaque++;
+      alert(
+        "Estás golpeando mucho. Dejemos que la suerte juegue ahora. Selecciona sí o sí la opción 4.",
+      );
+      return;
+    }
+
+    if (
+      typeof opcionAtaque !== "number" ||
+      opcionAtaque < 0 ||
+      opcionAtaque > 4 ||
+      isNaN(opcionAtaque)
+    ) {
+      alert("❌ Acción inválida. Por favor, ingresa un número del 0 al 4.");
+      return;
+    }
+
+    let ataqueSeleccionado = "";
+
+    for (const ataque of this.ataques) {
+      const indiceAtaque = this.ataques.indexOf(ataque);
+      if (opcionAtaque === indiceAtaque) {
+        ataqueSeleccionado = ataque;
+      }
+    }
+    let estadoVida = null;
+    if (ataqueSeleccionado) {
+      estadoVida = enemigo.vida -= ataqueSeleccionado.daño;
+      if (estadoVida <= 0) {
+        estadoVida = 0;
+        enemigo.vida = estadoVida;
+      }
+      this.intentosDeAtaque++;
+      alert(
+        "💥Golpeaste al enemigo con: " +
+          ataqueSeleccionado.nombre +
+          "\n" +
+          "Vida enemiga: " +
+          enemigo.vida +
+          " %",
+      );
+      return;
+    } else {
+      alert("❌ No tienes disponible ese poder");
+      return;
+    }
+  }
+}
 
 // MOSTRAR MENÚ
+const validarAccion = (accion) => {
+  if (!Number(accion) || accion < 1 || accion > 5) {
+    alert("❌ Acción inválida. Por favor, ingresa un número del 1 al 5.");
+    return false;
+  }
+  return true;
+};
 const mostrarMenu = () => {
   const menuOpciones = [
     "1. Golpear",
@@ -17,11 +88,13 @@ const mostrarMenu = () => {
 
   return parseInt(
     prompt(
-      "-Tu vida está al: " +
-        vidaJugador +
+      jugador.nombre +
+        ", tiene vida al: " +
+        jugador.vida +
         "%\n" +
-        "-La vida de tu enemigo está al: " +
-        vidaEnemigo +
+        enemigo.nombre +
+        ", tiene vida al: " +
+        enemigo.vida +
         "%\n" +
         "¿Qué quieres hacer?\n" +
         menuOpciones.join("\n").trim(),
@@ -29,126 +102,73 @@ const mostrarMenu = () => {
   );
 };
 
-// VALIDAR ACCIÓN
-const validarAccion = (accion) => {
-  if (!Number(accion) || accion < 1 || accion > 5) {
-    alert("❌ Acción inválida. Por favor, ingresa un número del 1 al 5.");
-    return false;
-  }
-  return true;
-};
-
 // ATACAR
-const seleccionDeAtaque = (opcionAtaque) => {
-
-  const opcionEsNumero = Number(opcionAtaque);
-
-  if ( typeof opcionEsNumero !== "number" || opcionEsNumero < 0 || opcionEsNumero > 4) {
-    alert("❌ Acción inválida. Por favor, ingresa un número del 0 al 4.");
-    return;
-  }
-
-  let ataqueSeleccionado = "";
+const mostrarListadoAtaques = (ataques) => {
+  let listadoAtaques = "";
 
   for (const ataque of ataques) {
     const indiceAtaque = ataques.indexOf(ataque);
-    if (opcionEsNumero === indiceAtaque) {
-      ataqueSeleccionado = ataque;
-    }
+    listadoAtaques += indiceAtaque + ". " + ataque.nombre + "\n";
   }
-  return ataqueSeleccionado;
-};
-
-const mostrarListadoAtaques = () => {
-  let listado = "";
-
-  for (const ataque of ataques) {
-    const indiceAtaque = ataques.indexOf(ataque);
-    listado += indiceAtaque + ". " + ataque + "\n";
-  }
-  return listado;
-};
-
-const atacar = () => {
-  if (intentosDeAtaque === 2) {
-    intentosDeAtaque++;
-    alert(
-      "Estás golpeando mucho. Dejemos que la suerte juegue ahora. Selecciona sí o sí la opción 4.",
-    );
-    return;
-  } else if (intentosDeAtaque > 2) {
-    alert("Ya no puedes atacar más. Selecciona sí o sí la opción 4.");
-    return;
-  } else {
-    const listadoAtaques = mostrarListadoAtaques();
-    const opcionAtaque = prompt(listadoAtaques);
-    const ataqueSeleccionado = seleccionDeAtaque(opcionAtaque);
-    const existeAtaque = ataques.includes(ataqueSeleccionado);
-
-    if (existeAtaque) {
-      vidaEnemigo -= 20;
-      intentosDeAtaque++;
-      alert(
-        "💥Golpeaste al enemigo con: " +
-          ataqueSeleccionado +
-          "\n" +
-          "Vida enemiga: " +
-          vidaEnemigo +
-          " %",
-      );
-      return;
-    } else {
-      alert("❌ No tienes disponible ese poder");
-      return;
-    }
-  }
+  return listadoAtaques;
 };
 
 // CURARSE
 const curarse = () => {
-  if (intentosDeAtaque > 2) {
+  if (jugador.intentosDeAtaque > 2) {
+    jugador.intentosDeCuracion++
     alert("❌ No puedes curarte. Selecciona sí o sí la opción 4.");
     return;
   }
 
-  if (vidaJugador >= 50) {
+  if (jugador.vida >= 50) {
+    jugador.intentosDeCuracion++
     alert("❌ No seas nena!! Da un par de golpes más 😒");
     return;
   }
 
-  if (intentosDeCuracion === 2) {
+  if (jugador.intentosDeCuracion === 2) {
+    jugador.intentosDeCuracion++
     alert(
       "❌ Superaste la cantidad de intentos de cura. Dale, dale, a pelear!!! 😒",
     );
     return;
   }
 
-  vidaJugador = 100;
-  intentosDeCuracion++;
-  intentosDeAtaque = 0;
+  jugador.vida += 20;
+  jugador.intentosDeCuracion++;
+  jugador.intentosDeAtaque = 0;
 
-  alert("Te curaste. Muy bien!!! ❤️\nTu vida está al: " + vidaJugador + "%");
-  return
+  alert("Te curaste. Muy bien!!! ❤️\nTu vida está al: " + jugador.vida + "%");
+  return;
 };
 
 // HUIR
 const huir = () => {
-  if (vidaJugador > 50) {
+  if (jugador.vida > 50) {
     alert(
       "❌ Sos una gallina!! Todavía tienes suficiente vida para seguir peleando. Vamos!!",
     );
 
     return;
   }
-
+  alert(
+    "🏃 Escapaste de la pelea. 💀 PERDISTE!! \n" +
+      "En la consola presionando F12, veras las estadísticas finales",
+  );
+  console.log("Estadísticas:", "\nJugador:", jugador, "\nEnemigo:", enemigo);
   juegoEnMarcha = false;
-
-  alert("🏃 Escapaste de la pelea. 💀 PERDISTE!!");
 };
 
 // POCIÓN MÁGICA
 const validarNumero = (valorAleatorio, minimo, maximo) => {
-  return valorAleatorio >= minimo && valorAleatorio <= maximo;
+  if (isNaN(valorAleatorio)) {
+    return valorAleatorio;
+  }
+  return (
+    (valorAleatorio >= minimo && valorAleatorio <= maximo) ||
+    isNaN(valorAleatorio)
+  );
 };
 
 const usarPocionMagica = () => {
@@ -161,43 +181,51 @@ const usarPocionMagica = () => {
   const numeroEsValido = validarNumero(valorAleatorio, 1, 10);
 
   if (!numeroEsValido) {
-    alert("❌" + valorAleatorio + " es un número incorrecto");
+    alert("❌" + valorAleatorio + " es un número o caracter incorrecto");
     return;
   }
-  if(valorAleatorio === 5 || valorAleatorio === 3 || valorAleatorio === 8){
-    alert("La suerte estuvo de parte de los dos; se salvaron!!")
-    intentosDeAtaque = 0;
-    return
+
+  if (valorAleatorio === 5 || valorAleatorio === 3 || valorAleatorio === 8) {
+    alert("La suerte estuvo de parte de los dos; se salvaron!!");
+    jugador.intentosDeAtaque = 0;
+    return;
   }
 
+  let resultadoVida = null;
   if (valorAleatorio > 5) {
-    vidaJugador -= 70;
-    intentosDeAtaque = 0;
-    const elementoEliminado = ataques.pop();
+    resultadoVida = jugador.vida -= 70;
+
+    if (resultadoVida <= 0) {
+      resultadoVida = 0;
+      jugador.vida = resultadoVida;
+    }
+    jugador.intentosDeAtaque = 0;
+    enemigo.intentosDeAtaque++;
+    const elementoEliminado = jugador.ataques.pop();
 
     alert(
       "Ufff, Ouch!! Lo siento, tu nivel de vida está al: " +
-        vidaJugador +
+        jugador.vida +
         "%" +
         "\n" +
         "Ademas perdiste un poder: " +
-        elementoEliminado,
+        elementoEliminado.nombre,
     );
 
     return;
   }
 
   if (valorAleatorio < 5) {
-    vidaEnemigo -= 30;
-    const nuevoAtaque = "Lanza";
-    ataques.push(nuevoAtaque);
+    enemigo.vida -= 30;
+    const nuevoAtaque = { nombre: "Lanza", daño: 15 };
+    jugador.ataques.push(nuevoAtaque);
 
     alert(
       "Golpeaste al enemigo brutalmente, Quedó con vida al: " +
-        vidaEnemigo +
+        enemigo.vida +
         "% \n" +
         "Ademas ganaste un nuevo ataque: " +
-        nuevoAtaque,
+        nuevoAtaque.nombre,
     );
     return;
   }
@@ -214,20 +242,35 @@ const salirDelJuego = () => {
 
 // COMPROBAR RESULTADO
 const comprobarEstadoDelJuego = () => {
-  if (vidaEnemigo <= 0) {
+  if (enemigo.vida <= 0) {
+    enemigo.vida = 0;
     juegoEnMarcha = false;
-    alert("🏆 ¡GANASTE!");
+    alert(
+      "🏆 ¡GANASTE! En la consola presionando F12, veras las estadísticas finales de la partida",
+    );
+    console.log("Estadísticas:", "\nJugador:", jugador, "\nEnemigo:", enemigo);
     return;
   }
 
-  if (vidaJugador <= 0) {
+  if (jugador.vida <= 0) {
+    jugador.vida = 0;
     juegoEnMarcha = false;
-    alert("💀 PERDISTE!!");
+    alert(
+      "💀 PERDISTE!! En la consola presionando F12, veras las estadísticas finales de la partida",
+    );
+    console.log("Estadísticas:\n", "Jugador:", jugador, "\nEnemigo:", enemigo);
     return;
   }
 };
 
 alert("⚔️ ¡COMIENZA LA PELEA!");
+
+let nombre = null;
+nombre
+
+const jugador = new Jugador("Oscar");
+const enemigo = new Jugador("enemigo");
+
 
 while (juegoEnMarcha) {
   const accion = mostrarMenu();
@@ -237,7 +280,8 @@ while (juegoEnMarcha) {
   if (esValida) {
     switch (accion) {
       case 1:
-        atacar();
+        const opcionAtaque = prompt(mostrarListadoAtaques(jugador.ataques));
+        jugador.atacar(Number(opcionAtaque));
         break;
 
       case 2:
